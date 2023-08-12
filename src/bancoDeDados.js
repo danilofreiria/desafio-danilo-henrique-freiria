fc7c92a6ba403b83ed3d7f2b6db397208c1dfc3b
Danilo Freiria <76266160+danilofreiria@users.noreply.github.com>
2023-08-12 15:26:45 -0300

    estruturação inicial

---
+++ src/bancoDeDados.js
@@ -0,0 +1,49 @@
+export const cardapio = {
+  principais: [
+    {
+      codigo: 'cafe',
+      descricao: 'Café',
+      valor: 3.00
+    },
+    {
+      codigo: 'suco',
+      descricao: 'Suco Natural',
+      valor: 6.20
+    },
+    {
+      codigo: 'sanduiche',
+      descricao: 'Sanduíche',
+      valor: 6.50
+    },
+    {
+      codigo: 'salgado',
+      descricao: 'Salgado',
+      valor: 7.25
+    }
+  ],
+  combos: [
+    {
+      codigo: 'combo1',
+      descricao: '1 Suco e 1 Sanduíche',
+      valor: 9.50
+    },
+    {
+      codigo: 'combo2',
+      descricao: '1 Café e 1 Sanduíche',
+      valor: 7.50
+    }
+  ],
+  extras: [
+    {
+      codigo: 'chantily',
+      descricao: 'Chantily (extra do Café)',
+      valor: 1.50
+    },
+    {
+      codigo: 'queijo',
+      descricao: 'Queijo (extra do Sanduíche)',
+      valor: 2.00
+    }
+  ]
+};
+
